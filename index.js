3018065ad3b58cd1e04870a1c90dd1b3046e4268
JSanoja <jsanoja@dosalcubo.com>
2019-02-13 13:20:46 -0300

    [TICKET:064326] TD4 - Dev

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'client/test-main.html');
 
 const server = express()
-  use(function(req, res, next) {
+  .use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
